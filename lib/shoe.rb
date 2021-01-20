ede75bdb4e6a8e1d6c78c8ba90fcf32c725ea81e
Joe Downes-Berry <j.d.berry26@gmail.com>
2021-01-20 18:35:48 -0500

    commit tree ddb25ae7fc747d4bd304b38d873b1306b1778de5

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,22 @@
 # Make your shoe class here!
+require 'pry'
+
+class Shoe
+    attr_accessor :color, :size, :material
+    attr_reader :brand, :condition
+    
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def cobble
+        
+        @condition = "new"
+        
+        puts "Your shoe is as good as new!"
+    end
+
+    def condition=(condition)
+        @condition = condition
+    end
+end
